7f0bf828b6dc8c8734384205b192ed8d0cf9644c
izajasz13 <karoldd13@gmail.com>
2019-08-27 21:56:06 +0200

    Finished api js functions for current and forecast weather

--- api.js
+++ api.js
@@ -1,12 +1,59 @@
-async function apiCall(city = 'London'){
+//API functions
+async function currentWeatherCall(city){
     let apiSite = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8a615664a0840a23b03c4418f16b9b13`;
     let response = await fetch(apiSite);
-    let data = await response.json().then(data => data.name);
+    let data = await response.json();
     return data;
 }
 
-let weatherData = apiCall().then(data => console.log(data))
+async function forecastWeatherCall(city, country){
+    let apiSite = `http://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&appid=8a615664a0840a23b03c4418f16b9b13`;
+    let response = await fetch(apiSite);
+    let data = await response.json();
+    return data;
+}
 
+async function weatherCheck(city = 'London', country = 'GB', select = '2'){
+    let weatherData;
+    switch(select){
+        case '1':
+            //Current weather access
+            weatherData = await currentWeatherCall(city).then(data => {
+                let info = {
+                    city: data.name,
+                    country: data.sys.country,
+                    temp: data.main.temp,
+                    cloud: data.weather[0].description,
+                    pres: data.main.pressure
+                };
+                //console.log(data);
+                return info;
+            });
+            break;
+        case '2':
+            //Each 3 hour timestamp weather access
+            weatherData = await forecastWeatherCall(city, country).then(data =>{
+                let info = {
+					city: data.city.name,
+					country: data.city.country,
+					first: [data.list[0].dt_txt, data.list[0].main.temp, data.list[0].main.pressure, data.list[0].weather[0].description],
+					second: [data.list[1].dt_txt, data.list[1].main.temp, data.list[1].main.pressure, data.list[1].weather[0].description],
+					third: [data.list[2].dt_txt, data.list[2].main.temp, data.list[2].main.pressure, data.list[2].weather[0].description],
+					fourth: [data.list[3].dt_txt, data.list[3].main.temp, data.list[3].main.pressure, data.list[3].weather[0].description]
+                };
+                //console.log(data);
+                return info;
+            });
+            break;
+        default:
+            console.log('Wrong select argument');
+            break;
+    }
+    return weatherData;
+}
+weatherCheck('Wroclaw', 'PL').then(obj => console.log(obj));
+
+//Search bar 
 const endpoint = 'https://raw.githubusercontent.com/mkolodziej19/Weather/master/city.list.min.json';
 
 const cities = [];
